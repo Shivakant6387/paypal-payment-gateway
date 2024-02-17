fc8125854a74594a0fa7955077192fde31820d2c
Shivakant6387 <shivakantsingh900@gmail.com>
2024-02-17 11:06:46 +0530

    nothing to commit

---
+++ src/test/java/com/example/paypal/payment/gateway/PaypalPaymentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paypal.payment.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalPaymentGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
